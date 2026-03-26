b972b9351de646373f0f01c5bb2fcd0711bcf906
Shailja <shailjarathore.2006@gmail.com>
2026-03-26 21:57:34 +0530

    updated frontend

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     #Security
     SECRET_KEY = "nexus-project"
     SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:Mysql%40121@localhost:3306/student_performance_db"
+        "mysql+pymysql://root:@localhost/student_performance_db"
     )
 
     # Disable event tracking
